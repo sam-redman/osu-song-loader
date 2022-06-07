7a41f2948ea2be669716425360a66022481b254f
SammoWhammo <sam.redman97@gmail.com>
2022-06-07 22:02:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# osu! Song Loader
+Load multiple osu! .osz songs from a directory.
+
+## Table of Contents
+* [Getting Started](#getting-started)
+* [Prerequisites](#prerequisites)
+* [Running the Application Locally](#running-the-application-locally)
+* [Usage](#usage)
+
+## Getting Started
+Once the program is run you are greeted with a simple GUI interface. As long as you know what directory downloaded .osz are stored in,
+you are good to go.
+
+### Prerequisites
+* [Python](https://www.python.org/downloads/)
+* [osu!](https://osu.ppy.sh/home)
+
+### Running the Application Locally
+````
+# Run the program.
+python main.py
+````
+
+## Usage
+This program is useful for loading osu! .osz from a directory automatically. By default, in order to load .osz into osu! you would need to
+double-click song files to load them. With this program you can download all your favourite osu! beatmaps into one directory and then use the
+simple GUI to load them all with a click of a button.
+
+![](docs/images/Demo.PNG)
---
+++ src/main.py
@@ -0,0 +1,54 @@
+# ---------------------------------- MODULES ---------------------------------- #
+
+import os
+import glob
+import time
+import tkinter.filedialog
+from tkinter import *
+
+# ---------------------------------- MAIN ---------------------------------- #
+
+
+def load():
+    """Load .osz files in the directory with a 2 second delay between each file load."""
+
+    # Grab the entry in the directory entry box.
+    directory = directory_entry.get()
+    # Loop through the directory and open each file. Wait two seconds between each file.
+    for filepath in glob.glob(os.path.join(directory, '*.osz')):
+        os.startfile(filepath)
+        time.sleep(2)
+
+
+def chose_directory():
+    """Let the user pick the folder that the .osz file are saved in."""
+
+    # Opens a dialog box to allow for easy navigation to the file directory.
+    chosen_directory = tkinter.filedialog.askdirectory()
+    # Insert the chosen directory in text format in the entry box.
+    directory_entry.insert(0, chosen_directory)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+# Create window.
+window = Tk()
+window.title("osu! Song Loader")
+# Padding, background colour.
+window.config(padx=50, pady=50)
+
+# Directory.
+directory_button = Button(width=11, text="Song Directory", command=chose_directory)
+directory_button.grid(column=1, row=1)
+
+directory_entry = Entry(width=40)
+directory_entry.grid(column=2, row=1, padx=5)
+
+# Loader.
+load_song_button = Button(width=11, text="Load", command=load)
+load_song_button.grid(column=1, row=2, pady=5)
+
+# ---------------------------- RUN ------------------------------- #
+
+# Keeps window on screen.
+window.mainloop()
